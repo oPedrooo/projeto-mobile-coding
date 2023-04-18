7e9b2b72b1512e2b11882e114dffc8187109c339
PEDRO ALVES <pedroalvesblira@gmail.com>
2023-04-18 18:40:43 -0300

    projeto mobile coding V0.2

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
@@ -7,18 +7,42 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       
-      <View style={styles.header}>
-        <View style={styles.clogo}>
-          <Ionicons style={styles.imenu} name="menu" size={35} color="black" />
-          <Text style={styles.Ltexto} >LOGO DA LOJA</Text>
+      {/* Header com a logo da loja e ícones */}
+      <View style={styles.header}> 
+
+        <View style={styles.HparteDeCima}>
+          {/* Caixa com "menu" e logo */}
+          <View style={styles.clogo}>
+            <Ionicons style={styles.imenu} name="menu" size={35} color="black" />
+            <Text style={styles.Ltexto} >LOGO DA LOJA</Text>
+          </View>
+          {/* Caixa com ícones de pesquisa e carrinho */}
+          <View style={styles.cicones}>
+            <Ionicons style={styles.ilupa} name="search" size={30} color="black" />
+            <Ionicons style={styles.icarrinho} name="cart" size={30} color="black" />
+          </View>  
         </View>
-        <View style={styles.cicones}>
-          <Ionicons style={styles.ilupa} name="search" size={30} color="black" />
-          <Ionicons style={styles.icarrinho} name="cart" size={30} color="black" />
+        
+        {/* Parte de baixo da Header com os atalhos diferentes tipos de produtos */}
+        <View style={styles.HparteDeBaixo}>
+          <Text style={styles.atalhoHigh}>Novos lançamentos</Text>
+          <Text>Camisas</Text>
+          <Text>Calças</Text>
+          <Text>Bolsas</Text>
+          <Text>Sapatos</Text>
         </View>
       </View>
 
-      <View style={styles.body} />
+      {/* Corpo do app */}
+      <View style={styles.body}>
+        {/*<ScrollView>
+          <View>
+            <Text style={styles.textoCorpo}>NOVOS LANÇAMENTOS</Text>
+            <Ionicons  />
+          </View>
+          
+  </ScrollView>   */}
+      </View>
 
     </View>
   );
@@ -35,27 +59,49 @@ const styles = StyleSheet.create({
   header: {
     flex: 1,
     backgroundColor: "white",
+    flexDirection: 'column',
+  },
+  HparteDeCima: {
+    flex:3,
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  HparteDeBaixo: {
+    flex: 1,
+    backgroundColor: 'white',
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
   clogo: {
     flex:4,
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    marginTop: 23,
   },
   Ltexto: {
-    fontSize: 25
+    fontSize: 25,
+    
+    marginTop: 3,
   },
   cicones: {
     flex:1,
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'flex-start',
+    marginTop: 23,
     justifyContent: 'space-around'
+  },
+  atalhoHigh: {
+    backgroundColor: 'grey',
   },
 
 
 
   body: {
-    flex: 8,
-    backgroundColor: "grey",
+    flex: 7,
+    backgroundColor: "white",
+  },
+  textoCorpo: {
+    fontSize: 30,
   },
 });
--- Readme.md
+++ Readme.md
@@ -1,1 +1,5 @@
 A ideia é montar uma tela de um app de e-commerce
+
+exemplo de e-commerce a ser usado: loja de roupas
+
+V 0.2 = Header praticamente finalizada + inicio de planejamento do body 
